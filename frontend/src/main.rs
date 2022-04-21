64ea0dce45ef1c4c9dffa85741dcce43149145a4
jhauter <jonathan_sascha.hauter@fh-bielefeld.de>
2022-04-21 23:11:17 +0200

    added yew dep, added basic yew project.

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,18 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    let page = html! {
+        <body>
+            <main class ="container">
+            <h1>{"Heya!"}</h1>
+            <article>{"We're working"}</article>
+            </main>
+        </body>
+    };
+    page
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<App>();
 }
