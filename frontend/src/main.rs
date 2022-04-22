aa977c0e737cf883c71b40c8167d54f04c0e48a0
jhauter <jonathan_sascha.hauter@fh-bielefeld.de>
2022-04-22 12:34:24 +0200

    added basic form, added some css.

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -4,10 +4,26 @@ use yew::prelude::*;
 fn app() -> Html {
     let page = html! {
         <body>
-            <main class ="container">
-            <h1>{"Heya!"}</h1>
-            <article>{"We're working"}</article>
-            </main>
+        <main class = "container">
+            <article>
+                <div>
+                  <hgroup>
+                    <h1>{"Registrieren"}</h1>
+                    <h2>{"Neuen Account erstellen"}</h2>
+                  </hgroup>
+                  <form>
+                    <div class = "grid">
+                        <input type="text" name="username" placeholder="Nutzername" aria-label="Login" autocomplete="nickname"/>
+                        <input type="text" name="email" placeholder="E-Mail Adresse" aria-label="Login" autocomplete="email"/>
+                    </div>
+
+                    <input type="text" name="cit_code" placeholder="Registrierungsschlüssel" aria-label="Registrierungsschlüssel"/>
+                    <input type="password" name="password" placeholder="Passwort" aria-label="Password" autocomplete="current-password"/>
+                    <button type="button" class="contrast">{"Bestätigen"}</button>
+                  </form>
+                </div>
+            </article>
+        </main>
         </body>
     };
     page
