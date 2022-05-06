3f151dd675a89bf41f7bfaa27decbeb82f936c62
Jonathan <joh2001@protonmail.com>
2022-05-06 10:38:30 +0200

    added basic moonzoon stuff, migrated basic registration page

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,19 +1,18 @@
-use actix_web::{get, http::Method, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use config::Config;
-use std::net::TcpListener;
-use SmartCity_Auth::{server_start, ServerConfig};
-#[actix_web::main]
+use moon::*;
 
+async fn frontend() -> Frontend {
+    Frontend::new()
+        .title("MZ Demo")
+        .default_styles(false)
+        .append_to_head(r#"<link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">"#)
+        .append_to_head(r#"<link rel="stylesheet" href="/_api/public/custom.css">"#)
+        .body_content(r#"<div id="app"></div>"#)
+}
+
+
+async fn up_msg_handler(_: UpMsgRequest<()>) {}
+
+#[moon::main]
 async fn main() -> std::io::Result<()> {
-    let server_config = Config::builder()
-        .add_source(config::File::with_name("config/server.toml"))
-        .build()
-        .unwrap();
-
-    let server_config = server_config.try_deserialize::<ServerConfig>().unwrap();
-
-    let listener = TcpListener::bind("127.0.0.1:0").unwrap();
-    let port = listener.local_addr().unwrap().port();
-
-    server_start(server_config, listener)?.await
+    start(frontend, up_msg_handler, |_| {}).await
 }
---
+++ frontend/src/lib.rs
@@ -0,0 +1,92 @@
+use rand::prelude::*;
+use std::sync::{
+    atomic::{AtomicUsize, Ordering},
+    Arc,
+};
+use std::{array, iter::repeat_with, ops::Not};
+use std::any::Any;
+use zoon::{format, *};
+use zoon::dominator::html;
+use zoon::named_color::{GREEN_7, GREEN_8};
+use zoon::Tag::{Article, H1, H2, Main};
+
+pub struct UserInfo {
+    username: String
+}
+
+fn page() -> impl Element {
+    RawHtmlEl::new("body")
+        .class("container")
+        .child(
+            RawHtmlEl::new("main")
+                .class("container")
+                .child(
+                    RawHtmlEl::new("article")
+                        .child(
+                            RawHtmlEl::new("div")
+                                .child(heading_group())
+                                .child(register_form())
+                        )
+                )
+        )
+}
+
+fn heading_group() -> impl Element {
+    RawHtmlEl::new("hgroup")
+        .child(RawHtmlEl::new("h1").child("Registrieren"))
+        .child(RawHtmlEl::new("h2").child("Neuen Account erstellen"))
+}
+
+fn register_form() -> impl Element {
+    RawHtmlEl::new("Form")
+        .child(
+            RawHtmlEl::new("div")
+                .class("grid")
+                .child(username_input())
+                .child(mail_input())
+        )
+        .child(cit_id_input())
+        .child(pw_input())
+        .child(submit_button())
+}
+
+pub struct UsernameInput {
+    username: Mutable<String>,
+    raw: RawHtmlEl,
+
+}
+fn username_input() -> impl Element {
+    RawHtmlEl::new("input")
+        .attr("type", "text")
+        .attr("name", "login")
+}
+fn mail_input() -> impl Element {
+    RawHtmlEl::new("input")
+        .attr("type", "text")
+        .attr("name", "login")
+
+}
+fn cit_id_input() -> impl Element {
+    RawHtmlEl::new("input")
+        .attr("placeholder", "Registrierungsschlüssel")
+}
+
+fn pw_input() -> impl Element {
+    RawHtmlEl::new("input")
+        .attr("placeholder", "password")
+}
+
+fn submit_button() -> impl Element {
+    RawHtmlEl::new("button")
+        .attr("type", "button")
+        .attr("class", "contrast")
+        .child("Registrieren")
+}
+// ------ ------
+//     Start
+// ------ ------
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    start_app("app", page);
+}
