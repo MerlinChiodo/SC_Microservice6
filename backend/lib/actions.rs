e827752fe02bbb8ef732c8f1e1d178550f690f0a
jhauter <jonathan_sascha.hauter@fh-bielefeld.de>
2022-05-30 18:58:58 +0200

    added very simple token based session system

--- backend/lib/actions.rs
+++ backend/lib/actions.rs
@@ -1,10 +1,12 @@
 use diesel::{ExpressionMethods, MysqlConnection, QueryDsl, RunQueryDsl};
 use diesel_migrations::name;
+use diesel::associations;
 use rand::{Rng, RngCore};
-use crate::models::{NewUser, User, UserInfo};
+use crate::models::{NewSession, NewUser, Session, User, UserInfo};
+use crate::schema::Sessions::dsl::Sessions;
 use crate::schema::Users::dsl::Users;
 use crate::schema::Users::username;
-
+use crate::diesel::BelongingToDsl;
 #[derive(Debug)]
 pub enum UserRegistrationError {
     HashError(argon2::Error),
@@ -19,10 +21,24 @@ pub enum UserAuthError {
     WrongPassword
 }
 
+#[derive(Debug)]
+pub enum SessionCreationError {
+    DbError(diesel::result::Error),
+}
+
+#[derive(Debug)]
+pub enum SessionRetrieveError {
+    DbError(diesel::result::Error),
+    NoSessionFound,
+    SessionExpired,
+}
+
 pub fn insert_new_user(db: &MysqlConnection, user: UserInfo) -> Result<(), UserRegistrationError> {
     let new_user = NewUser::try_from(user)
         .map_err(|err| UserRegistrationError::HashError(err))?;
 
+
+    //TODO: Handle specific insertion errors
     diesel::insert_into(Users)
         .values(&new_user)
         .execute(db)
@@ -35,10 +51,35 @@ pub fn get_user(db: &MysqlConnection, user: &UserInfo) -> Result<User, UserAuthE
         .limit(1)
         .load::<User>(db)
         .map_err(|err| UserAuthError::DbError(err))?;
-    let user_result = results.pop().ok_or(UserAuthError::UserNotFound)?;
+    let user_result = results.pop()
+        .ok_or(UserAuthError::UserNotFound)?;
 
-    let password_correct = user_result.verify_with_password(user.password.as_str())
+    let password_correct = user_result
+        .verify_with_password(user.password.as_str())
         .map_err(|err| UserAuthError::VerifyError(err))?;
 
     if password_correct {Ok(user_result)} else {Err(UserAuthError::WrongPassword)}
 }
+
+pub fn insert_new_session(db: &MysqlConnection, user: &User) -> Result<(), SessionCreationError> {
+    let session = NewSession::new(user);
+
+    diesel::insert_into(Sessions)
+        .values(&session)
+        .execute(db)
+        .map_err(|err| SessionCreationError::DbError(err))?;
+    Ok(())
+}
+
+pub fn get_session(db: &MysqlConnection, user: &User) -> Result<Session, SessionRetrieveError> {
+    //TODO: Handle cases where entry was not found differently
+    let session = Session::belonging_to(user)
+        .first(db)
+        .map_err(|err| {SessionRetrieveError::DbError(err)})?;
+
+    if session.is_valid{(Ok(session))} else {Err(SessionRetrieveError::SessionExpired)}
+
+}
+
+
+
--- backend/lib/schema.rs
+++ backend/lib/schema.rs
@@ -1,7 +1,23 @@
 table! {
+    Sessions (id) {
+        id -> Unsigned<Bigint>,
+        user_id -> Unsigned<Bigint>,
+        token -> Varchar,
+        expires -> Datetime,
+    }
+}
+
+table! {
     Users (id) {
         id -> Unsigned<Bigint>,
         username -> Varchar,
         hash -> Varchar,
     }
 }
+
+joinable!(Sessions -> Users (user_id));
+
+allow_tables_to_appear_in_same_query!(
+    Sessions,
+    Users,
+);
