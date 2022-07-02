3b59da3569a6ecc888bb9c65759d96687decfa51
jhauter <jonathan_sascha.hauter@fh-bielefeld.de>
2022-07-02 14:56:31 +0200

    fixed issue with citizen info parsing part 3

--- backend/lib/auth/Citizen.rs
+++ backend/lib/auth/Citizen.rs
@@ -15,7 +15,6 @@ pub struct CitizenAddress {
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct CitizenInfo {
-    pub citizen_id: u64,
     pub firstname: String,
     pub lastname: String,
     pub gender: Option<String>,
